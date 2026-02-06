763fc5b5ca9f284a606d4fd25d7bc3d32ac35018
Simerdeep <simerdeepsingh567@gmail.com>
2026-02-07 02:31:38 +0530

    to do list completed

--- script.js
+++ script.js
@@ -20,53 +20,74 @@ function openFeatures() {
 }
 openFeatures()
 
-let form = document.querySelector(".addTask form")
-let taskInput = document.querySelector(".addTask form input")
-let taskDetailInput = document.querySelector(".addTask form textarea")
-let taskCheckbox = document.querySelector(".addTask form #check")
+function todoList() {
+    let form = document.querySelector(".addTask form")
+    let taskInput = document.querySelector(".addTask form input")
+    let taskDetailInput = document.querySelector(".addTask form textarea")
+    let taskCheckbox = document.querySelector(".addTask form #check")
 
-let currentTask = []
+    let currentTask = []
 
-if(localStorage.getItem('currentTask')){
-    currentTask = JSON.parse(localStorage.getItem('currentTask'))
-} else{
-    console.log("Task list is empty");
-}
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'))
+    } else {
+        console.log("Task list is empty");
+    }
 
 
-function addTask(){
-    form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    currentTask.push({
-        task: taskInput.value,
-        details: taskDetailInput.value,
-        imp: taskCheckbox.checked
-    })
-    localStorage.setItem('currentTask',JSON.stringify(currentTask))
-    form.reset()
+    function addTask() {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            currentTask.push({
+                task: taskInput.value,
+                details: taskDetailInput.value,
+                imp: taskCheckbox.checked
+            })
+            renderTask()
+            location.reload()
+        })
+    }
+
+    function renderTask() {
+        localStorage.setItem('currentTask', JSON.stringify(currentTask))
+        let allTask = document.querySelector('.allTask')
+
+
+        let sum = ''
+
+        currentTask.forEach((elem, id) => {
+            sum += `
+        <div class="task">
+            <h5>
+                ${elem.task}
+                <span class="${elem.imp}">imp</span>
+            </h5>
+
+            <details>
+                <summary>View details</summary>
+                <p>${elem.details}</p>
+            </details>
+
+            <button id=${id}>Mark as completed</button>
+        </div>
+    `;
+        });
+
+
+        allTask.innerHTML = sum
+    }
+
     renderTask()
-})
-}
+    addTask()
 
-function renderTask() {
-    let allTask = document.querySelector('.allTask')
+    let markCompletedBtn = document.querySelectorAll('.task button')
 
-
-    let sum = ''
-
-    currentTask.forEach((elem) => {
-        sum += `<div class="task">
-                        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
-                        <p>${elem.details}</p>
-                        <button>Mark as completed</button>
-                    </div>`
+    markCompletedBtn.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            currentTask.splice(btn.id, 1)
+            renderTask()
+            location.reload()
+        })
     })
-
-
-    allTask.innerHTML = sum
-
 }
-
-renderTask()
-addTask()
-
+todoList()
